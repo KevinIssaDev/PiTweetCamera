41913893410cc707040dd9ca2e8b5cf56375fef5
Kevin Issa <me@kevinissa.dev>
2019-05-20 17:58:41 +0200

    Update pitweetcamera.py

--- pitweetcamera.py
+++ pitweetcamera.py
@@ -1,23 +1,26 @@
-from gpiozero import Button, LED
+from gpiozero import Button, LED, TrafficLights, MotionSensor
 from picamera import PiCamera
-from time import sleep
 from datetime import datetime
 from signal import pause
 from json import load
 import tweepy
-#sudo pip3 install tweepy
+
 
 def vrint(content):
     """ Verbose printing for debugging """
-    if verbose == True:
+    if verbose:
         print(content)
 
-def timestamp_filename(timestamp):
-    """ Formats a filename from timestamp
 
-    'day-month-year@hour;minute.png'
-    """
-    return timestamp.strftime('%d-%m-%Y@%H;%M.png')
+def timestamp_format(timestamp, filename=False, ext="jpg"):
+    """ Formats timestamp to string """
+    if filename:
+        # 'day-month-year@hour;minute.jpg'
+        return timestamp.strftime('%d-%m-%Y@%H;%M;%S.' + ext)
+    else:
+        # Day Month Date Hour:Minute:Second year
+        return timestamp.strftime('%c')
+
 
 def load_auth_tokens(auth_file):
     """ Loads the authentication tokens from 'auth_file' """
@@ -26,23 +29,63 @@ def load_auth_tokens(auth_file):
     vrint("Authentication tokens loaded.")
     return auth_tokens
 
-def tweet(api, media = False, media_path = None, content):
+
+def tweet(content, media=False, media_path=None):
     """ Tweets the content with or without media """
     if media:
         api.update_with_media(media_path, content)
     else:
         api.update_status(content)
 
-# def capture_photo():
-#     pass
+
+def on_motion():
+    """ Takes a photo then records until movement stops """
+    lights.red.off()
+    lights.amber.blink()
+    vrint("Motion detected!")
+    thumbnail_capture_time = datetime.now()
+    thumbnail_filename = timestamp_format(thumbnail_capture_time, filename=True)
+    camera.capture(thumbnail_filename)
+    vrint("Thumbnail captured.")
+    vrint("Recording...")
+    camera.start_recording(timestamp_format(datetime.now(), filename=True, ext='h264'))
+    tweet("Motion detected at {}!".format(timestamp_format(thumbnail_capture_time)), media=True, media_path=thumbnail_filename)
+    vrint("Posted to Twitter.")
+
+
+def on_no_motion():
+    """ Stops the recording when there's no movement """
+    lights.amber.off()
+    lights.red.on()
+    camera.stop_recording()
+    vrint("Recording done.")
+
+
+def on_button_press():
+    """ Takes a photo when the button is pressed """
+    lights.red.off()
+    lights.green.on()
+    vrint("Photo captured.")
+    camera.capture(timestamp_format(datetime.now(), filename=True))
+    lights.green.off()
+    lights.red.on()
 
 
 if __name__ == "__main__":
-    #button = Button(14)
-    #camera = PiCamera(14)
+    button = Button(17)
+    sensor = MotionSensor(15)
+    lights = TrafficLights(2, 3, 4)
+    lights.red.off()
+    lights.amber.off()
+    lights.green.off()
+    camera = PiCamera()
     verbose = True
     auth_tokens = load_auth_tokens('twitter_auth.json')
     auth = tweepy.OAuthHandler(auth_tokens['consumer_key'], auth_tokens['consumer_secret'])
     auth.set_access_token(auth_tokens['access_token'], auth_tokens['access_token_secret'])
     api = tweepy.API(auth)
-    #timestamp_filename(datetime.now())
+    lights.red.on()
+    sensor.when_motion = on_motion
+    sensor.when_no_motion = on_no_motion
+    button.when_pressed = on_button_press
+    pause()
